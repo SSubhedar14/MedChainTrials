3810cdb0809523e14c0a1b41e1b0be6f7d8092e2
Sharvari <sharvarisubhedar2000@gmail.com>
2024-09-28 12:56:27 +0530

    Initial commit with project files

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-# # Function to authorize researcher
-# def authorize_researcher(account):
-#     tx_hash = contract.functions.authorizeResearcher(account).transact({'from': account})
-#     receipt = web3.eth.wait_for_transaction_receipt(tx_hash)
-#     return receipt
